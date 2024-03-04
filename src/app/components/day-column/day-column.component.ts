0888bdd44a3ec5a7ce563a2de4cd06d5315a2285
oliver.nikolovski <oliver.nikolovski@sorsix.com>
2024-03-04 21:31:34 +0100

    Introducing mouse events for drawing calendar events

--- src/app/components/day-column/day-column.component.ts
+++ src/app/components/day-column/day-column.component.ts
@@ -1,4 +1,4 @@
-import {booleanAttribute, Component, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {WeekDayPipe} from "../../pipes/week-day.pipe";
 
 @Component({
@@ -8,8 +8,33 @@ import {WeekDayPipe} from "../../pipes/week-day.pipe";
     WeekDayPipe
   ],
   templateUrl: './day-column.component.html',
-  styleUrl: './day-column.component.scss'
+  styleUrl: './day-column.component.scss',
+  host: {
+    '(mousedown)': 'onmousedown($event)',
+    '(mouseup)': 'onmouseup($event)',
+    '(mousemove)': 'onmousemove($event)'
+  }
 })
 export class DayColumnComponent {
   @Input({ required: true }) date!: Date;
+  draggable: boolean = false;
+  start: number | null = null;
+  end: number | null = null;
+  y: number | null = null;
+
+  onmousedown(event: MouseEvent) {
+    this.draggable = true;
+    this.start = event.y;
+  }
+
+  onmouseup(event: MouseEvent) {
+    this.draggable = false;
+    this.end = event.y;
+  }
+
+  onmousemove(event: MouseEvent) {
+    if (this.draggable) {
+      console.log('x:', event.x, 'y:', event.y);
+    }
+  }
 }
