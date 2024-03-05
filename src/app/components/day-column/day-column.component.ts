f7bb846389da7ae7d929efcd330c997f39339be8
oliver.nikolovski <oliver.nikolovski@sorsix.com>
2024-03-05 21:18:29 +0100

    renderer2

--- src/app/components/day-column/day-column.component.ts
+++ src/app/components/day-column/day-column.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, Renderer2, ViewChild} from '@angular/core';
 import {WeekDayPipe} from "../../pipes/week-day.pipe";
 
 @Component({
@@ -15,16 +15,33 @@ import {WeekDayPipe} from "../../pipes/week-day.pipe";
     '(mousemove)': 'onmousemove($event)'
   }
 })
-export class DayColumnComponent {
+export class DayColumnComponent implements AfterViewInit {
   @Input({ required: true }) date!: Date;
   draggable: boolean = false;
   start: number | null = null;
   end: number | null = null;
-  y: number | null = null;
+  //y: number | null = null;
+  eventDiv: HTMLElement | null = null;
+
+  @ViewChild('area') area!: ElementRef;
+
+  constructor(private _renderer: Renderer2) {
+  }
+
+  ngAfterViewInit() {
+    //console.log('area', this.area);
+  }
 
   onmousedown(event: MouseEvent) {
+    console.log('area', this.area);
     this.draggable = true;
     this.start = event.y;
+    this._renderer.setStyle(this.area.nativeElement, 'top', `${this.start}px`);
+    this._renderer.setStyle(this.area.nativeElement, 'width', `100%`);
+    // this.eventDiv = this._renderer.createElement('div') as HTMLElement;
+    // this._renderer.setStyle(this.eventDiv, 'background-color', 'blue');
+    // this._renderer.setStyle(this.eventDiv, 'width', '100%');
+    // this._renderer.setStyle(this.eventDiv, 'height', '0px');
   }
 
   onmouseup(event: MouseEvent) {
@@ -34,7 +51,9 @@ export class DayColumnComponent {
 
   onmousemove(event: MouseEvent) {
     if (this.draggable) {
-      console.log('x:', event.x, 'y:', event.y);
+      const height = event.y - this.start!!;
+      this._renderer.setStyle(this.area.nativeElement,  'height', `${height}px`);
+      //this._renderer.setStyle(this.eventDiv, 'height', `${height}px`);
     }
   }
 }
